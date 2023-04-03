a9fcf2975c16bae40accc819c1f52cb8f17b3ba1
Nguyen Manh Tan <tan@Nguyens-MacBook-Pro.local>
2023-04-03 17:17:35 +0200

    init agent

---
+++ agents/group21_agent/group21_agent.py
@@ -0,0 +1,9 @@
+from geniusweb.party.DefaultParty import DefaultParty
+
+class Group21Agent(DefaultParty):
+    def __init__(self, reporter: Reporter = None):
+        pass
+
+    # Acceptance Strategy
+
+    # Bidding Strategy
